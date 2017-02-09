ba642d78640c0f57e9278483e560ebd4244ee868
Lucius Humm <luzi@elage.ch>
2017-02-09 23:31:59 +0100

    add Seconds manipulation

---
+++ seconds.go
@@ -0,0 +1,21 @@
+package hammertime
+
+import (
+	"time"
+)
+
+func AddSecond(t time.Time) time.Time {
+	return AddSeconds(t, 1)
+}
+
+func SubSecond(t time.Time) time.Time {
+	return SubSeconds(t, 1)
+}
+
+func SubSeconds(t time.Time, n int) time.Time {
+	return AddSeconds(t, -n)
+}
+
+func AddSeconds(t time.Time, n int) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second()+n, t.Nanosecond(), t.Location())
+}
